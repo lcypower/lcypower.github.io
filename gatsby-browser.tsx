e69ca06d7e5b951f208a4b94fe3c307e3863f410
lcypower <lcypower0@gmail.com>
2025-01-03 14:26:29 +0900

    reset

--- gatsby-browser.tsx
+++
@@ -1,9 +0,0 @@
-import { GatsbyBrowser } from 'gatsby'
-import Layout from './src/components/common/Layout'
-
-export const wrapPageElement: GatsbyBrowser['wrapPageElement'] = ({
-  element,
-  props,
-}) => {
-  return <Layout {...props}>{element}</Layout>
-}
