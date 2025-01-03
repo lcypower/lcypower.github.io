a3855a175c0a2392cb5966c89ef814a4b17d3e47
lcypower <lcypower0@gmail.com>
2025-01-03 14:35:16 +0900

    Updates

---
+++ component---src-pages-404-js-12e56ddd6d245297592d.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunklcypower_github_io=self.webpackChunklcypower_github_io||[]).push([[883],{45:function(e,t,n){n.r(t);var u=n(7294),l=n(6495),o=n(3432);t.default=function(){return u.createElement(l.Z,null,u.createElement(o.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-12e56ddd6d245297592d.js.map
